bdeba127a9e5d7279da536b9bc133df69809bf09
Joe Z <Joseph.J.Ziminski@gmail.com>
2021-11-19 23:43:04 +0000

    updated recording mode on channel output

--- readers/data_reader.py
+++ readers/data_reader.py
@@ -109,7 +109,7 @@ def get_list_of_channel_info_dicts(num_channels, param_dict):
 
 def get_series_channels(pul, group_idx, series_idx):
 
-    param_dict = {"name": [], "unit": [], "dtype": [], "sampling_step": []}
+    param_dict = {"name": [], "unit": [], "dtype": [], "sampling_step": [], "recording_mode": []}
     series = pul["ch"][group_idx]["ch"][series_idx]
 
     num_channels = get_max_num_channels_in_series(pul, group_idx, series_idx)
@@ -124,6 +124,7 @@ def get_series_channels(pul, group_idx, series_idx):
             __, __, __, dtype = get_dataformat(rec["hd"]["TrDataFormat"])
             channels[rec_idx]["dtype"].append(dtype)
             channels[rec_idx]["sampling_step"].append(rec["hd"]["TrXInterval"])
+            channels[rec_idx]["recording_mode"].append(rec["hd"]["TrRecordingMode"])
 
     # CHECK CHANNELS ARE ALL THE SAME
     results = [{} for __ in range(num_channels)]
