bbd70ab2620a994373f56a3dcd1c163f095bf07d
Joe Z <Joseph.J.Ziminski@gmail.com>
2021-12-19 13:10:39 +0000

    made imports relative again

--- load_heka.py
+++ load_heka.py
@@ -1,20 +1,20 @@
 from io import open
 import numpy as np
 import struct
-from trees.SharedTrees import BundleHeader, BundleItems, UserParamDescrType, LockInParams_v9, AmplifierState_v9, UserParamDescrType, Description, \
+from .trees.SharedTrees import BundleHeader, BundleItems, UserParamDescrType, LockInParams_v9, AmplifierState_v9, UserParamDescrType, Description, \
     cstr, MarkerRootRecord, MarkerRecord, get_stim_to_dac_id, get_data_kind
-from readers import stim_reader
-from readers import data_reader
+from .readers import stim_reader
+from .readers import data_reader
 
 def _import_trees(header):
     """
     Import the relevant tree (v9 or v1000) based on header. TODO: Probably a nicer way to do this.
     """
     if header["oVersion"] in ["v2x90.2, 22-Nov-2016"]:  # "v2x73.5, 21-May-2015" TODO: account for different record sizes
-        from trees import Trees_v9 as Trees
+        from .trees import Trees_v9 as Trees
 
     elif header["oVersion"] in ["v2x90.5, 09-Apr-2019", "v2x91, 23-Feb-2021", "1.2.0 [Build 1469]"]:
-        from trees import Trees_v1000 as Trees
+        from .trees import Trees_v1000 as Trees
     else:
         raise Exception("Version not current supported, please contact support@easyelectrophysiology.com")
 
